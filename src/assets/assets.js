1ff341a2f7b2e517d7cfe0f9011665a4a47219ab
DannielVo <phattinhoc2017@gmail.com>
2025-05-31 20:19:02 +0700

    Feat: finish UI profile page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,6 @@
 import icon from "./Icon.png";
 import logo from "./Logo.png";
 import userProfile from "./user_profile.jpg";
+import profileIcon from "./profileIcon.png";
 
-export const assets = { icon, logo, userProfile };
+export const assets = { icon, logo, userProfile, profileIcon };
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./profile.css";
+import { assets } from "../../assets/assets";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="profile-wrapper">
+      {" "}
+      <div className="profile-container">
+        <a href="Home_2.html" className="back-icon">
+          &#8592;
+        </a>
+
+        <div className="avatar-wrapper">
+          <div className="avatar-image">
+            <label htmlFor="avatar-upload">
+              <img id="avatar-img" src={assets.profileIcon} />
+            </label>
+            <input type="file" id="avatar-upload" accept="image/*" />
+          </div>
+
+          <div className="avatar-actions">
+            <label
+              htmlFor="avatar-upload"
+              className="action-icon"
+              title="Edit avatar"
+            >
+              <i className="bx bx-edit edit-icon"></i>
+            </label>
+            <button
+              type="button"
+              id="delete-avatar"
+              className="action-icon"
+              title="Delete avatar"
+            >
+              <i className="bx bx-trash"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="info-group">
+          <label>Email</label>
+          <input type="email" />
+
+          <label>Full Name</label>
+          <input type="text" />
+        </div>
+
+        <button className="update-button">Update</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
