f3db8e570f1b99c5b6cc1af5747ec5db9a46b71d
DannielVo <phattinhoc2017@gmail.com>
2025-05-31 20:07:31 +0700

    Feat: finish UI sign up page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,51 @@ import React from "react";
 import "./signUp.css";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="signUp-wrapper">
+      <div className="signUp-container">
+        <h1 className="signUp-title">Sign Up</h1>
+
+        <form>
+          <div className="signUp-form-group">
+            <label htmlFor="fullname">Fullname</label>
+            <input type="text" id="fullname" name="fullname" required />
+          </div>
+
+          <div className="signUp-form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="signUp-form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+
+          <div className="signUp-form-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              required
+            />
+          </div>
+
+          {/* <!-- Button Sign up --> */}
+          <button type="submit" className="signUp-button">
+            Sign Up
+          </button>
+
+          <div className="login-link">
+            <p>
+              Already have an account? <a href="Login.html">Login</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
