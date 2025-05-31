2d4ecac514d570fe4149a83f8ae8c22ed0f556c6
DannielVo <phattinhoc2017@gmail.com>
2025-05-31 20:04:36 +0700

    Feat: finish UI login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,38 @@ import React from "react";
 import "./login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-wrapper">
+      <div className="login-container">
+        <h1 className="login-title">Login</h1>
+
+        <form>
+          <div className="login-form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="login-form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+
+          <div className="login-form-footer">
+            <button type="submit" className="forgot-password">
+              Forgot password
+            </button>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </div>
+
+          <div className="register-link">
+            Don't have an account? <a href="SignUp.html">Register now!</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
