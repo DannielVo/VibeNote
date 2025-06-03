040e7bcf92ea4d8ceb592057d0081f48eba1417e
DannielVo <phattinhoc2017@gmail.com>
2025-06-03 16:55:53 +0700

    Feat: finish UI for locked-note, locked-icon, pin-icon

--- src/components/NoteItem/NoteItem.jsx
+++ src/components/NoteItem/NoteItem.jsx
@@ -3,58 +3,101 @@ import "./noteItem.css";
 
 const NoteItem = () => {
   const [isLocked, setIsLocked] = useState(false);
+  const [isNoteMenuOpen, setIsNoteMenuOpen] = useState(false);
+  const [isLockedIconOpen, setIsLockedIconOpen] = useState(true);
+  const [isPinnedIconOpen, setIsPinnedIconOpen] = useState(true);
 
+  const togglePinnedIcon = () => {
+    setIsPinnedIconOpen((prev) => !prev);
+  };
+
+  const toggleLockedIcon = () => {
+    if (isLockedIconOpen) {
+      // Note lúc này chưa khóa => Cần khóa lại
+      // TODO: Hiện modal set password cho note + Khóa note lại
+      setIsLocked(true);
+    } else {
+      // Note lúc này đã khóa => Cần mở
+      // TODO: Hiện modal xác nhận password trc khi mở khóa + Mở note ra => Note trở lại UI bình thường
+      setIsLocked(false);
+    }
+    setIsLockedIconOpen((prev) => !prev);
+  };
+
+  const toggleNoteMenu = () => {
+    setIsNoteMenuOpen((prev) => !prev);
+  };
   return (
     <>
-      {isLocked ? (
-        <div className="note-card locked-note">
-          <i className="bx bx-lock locked-note-icon"></i>
-          <div className="note-options">
-            <i className="bx bx-dots-vertical-rounded"></i>
-          </div>
-        </div>
-      ) : (
-        <div className="note-card">
-          <h3 className="note-card-title">Monday</h3>
-          <div className="note-labels">
-            <span className="note-card-labels">Work</span>
-            <span className="note-card-labels">Project</span>
-            <span className="note-card-labels">Ideas</span>
-          </div>
-          <p className="note-card-content">Hello world!</p>
-          <div className="note-options">
-            <i className="bx bx-dots-vertical-rounded"></i>
-          </div>
+      <div className={`note-card ${isLocked ? "locked-note" : ""} `}>
+        {isLocked ? (
+          <>
+            <i className="bx bx-lock locked-note-icon"></i>
+            <div className="note-options">
+              <i
+                className="bx bx-dots-vertical-rounded"
+                onClick={toggleNoteMenu}
+              ></i>
+            </div>
+          </>
+        ) : (
+          <>
+            <h3 className="note-card-title">Monday</h3>
+            <div className="note-labels">
+              <span className="note-card-labels">Work</span>
+              <span className="note-card-labels">Project</span>
+              <span className="note-card-labels">Ideas</span>
+            </div>
+            <p className="note-card-content">Hello world!</p>
+            <div className="note-options">
+              <i
+                className="bx bx-dots-vertical-rounded"
+                onClick={toggleNoteMenu}
+              ></i>
+            </div>
+          </>
+        )}
 
-          {/* Popup note options */}
+        {/* Popup note options */}
+        {isNoteMenuOpen && (
           <div className="note-menu">
-            <div className="menu-item">
-              <i className="bx bx-pin"></i> Pin
-            </div>
-            <div className="menu-item">
-              <i className="bx bx-pin-slash-alt"></i> Unpin
-            </div>
+            {isPinnedIconOpen ? (
+              <div className="menu-item" onClick={togglePinnedIcon}>
+                <i className="bx bx-pin"></i> Pin
+              </div>
+            ) : (
+              <div className="menu-item" onClick={togglePinnedIcon}>
+                <i className="bx bx-pin-slash-alt"></i> Unpin
+              </div>
+            )}
+
             <div className="menu-item">
               <i className="bx bx-edit editNote-icon"></i> Edit
             </div>
             <div className="menu-item">
               <i className="bx bx-share"></i> Share
             </div>
-            <div className="menu-item">
-              <i className="bx bx-lock"></i> Lock
-            </div>
-            <div className="menu-item">
-              <i className="bx bx-lock-open-alt"></i> Unlock
-            </div>
-            <div className="menu-item">
-              <i className="bx bx-key"></i> Change password
-            </div>
+            {isLockedIconOpen ? (
+              <div className="menu-item" onClick={toggleLockedIcon}>
+                <i className="bx bx-lock"></i> Lock
+              </div>
+            ) : (
+              <>
+                <div className="menu-item" onClick={toggleLockedIcon}>
+                  <i className="bx bx-lock-open-alt"></i> Unlock
+                </div>
+                <div className="menu-item">
+                  <i className="bx bx-key"></i> Change password
+                </div>
+              </>
+            )}
+
             <div className="menu-item deleteNote-icon">
               <i className="bx bx-trash"></i> Delete
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
